f90048aeb710d4d59aae6105164b54c298b154f1
Yaser Sharifi <yaser.raadco@gmail.com>
2025-11-09 14:25:28 +0330

    feat: datatype - nil

--- data_types/data_types.go
+++ data_types/data_types.go
@@ -23,12 +23,27 @@ func main() {
 		FRI
 		SAT
 	)
+	const (
+		_ = iota // Discard the first value (0)
+		KB = 1 << (10 * iota) // 1 << (10 * 1)
+		MB = 1 << (10 * iota) // 1 << (10 * 2)
+		GB = 1 << (10 * iota) // 1 << (10 * 3)
+		TB = 1 << (10 * iota) // 1 << (10 * 4)
+	)
+	// nil
+	var arr []int = nil
 
-	fmt.Println("Is true: ", isTrue);
-	fmt.Println("Is false: ", isFalse);
-	fmt.Println("Sunday: ", SUN); // Output: 0
-	fmt.Println("Monday: ", MON); // Output: 1
-	fmt.Println("Friday: ", FRI); // Output: 5
+	fmt.Println("Is true: ", isTrue)
+	fmt.Println("Is false: ", isFalse)
+	fmt.Println("Sunday: ", SUN) // Output: 0
+	fmt.Println("Monday: ", MON) // Output: 1
+	fmt.Println("Friday: ", FRI) // Output: 5
+	fmt.Println("KB: ", KB) // Output: 1024
+	fmt.Println("MB: ", MB) // Output: 1048576
+	fmt.Println("GB: ", GB)
+	fmt.Println("TB: ", TB)
+	fmt.Println("Nil Array: ", arr)
+	fmt.Println("Is Nil: ", arr == nil)
 
 	/* Types (تایپ ها)
 		int, int8, int16, int32, int64, uint,
