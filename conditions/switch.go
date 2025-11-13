191d4388ca41e8c97a6190d97cfd97881a4518be
Yaser Sharifi <yassersharifi74@gmail.com>
2025-11-13 09:27:01 +0330

    feat: switch - fallthrough

--- conditions/switch.go
+++ conditions/switch.go
@@ -33,6 +33,78 @@ func main() {
 	fmt.Printf("Type of langs is: %s", typeNameOfLangs)
 
 	fmt.Println()
+
+	// by statement
+	switch webFramework := "React JS"; webFramework {
+	case "React JS", "Next JS":
+		fmt.Println("First need to learn React JS.")
+	case "Vue JS", "Nuxt Js":
+		fmt.Println("First need to learn Vue JS.")
+	default:
+		fmt.Println("I don't know this framework.")
+	}
+
+	fmt.Println()
+
+	// fallthrough
+	dayOfWeek := 5
+
+	switch dayOfWeek {
+	case 1:
+		fmt.Println("Monday")
+		fallthrough
+	case 2:
+		fmt.Println("Tuesday")
+		fallthrough
+	case 3:
+		fmt.Println("Wednesday")
+		fallthrough
+	case 4:
+		fmt.Println("Thursday")
+		fallthrough
+	case 5:
+		fmt.Println("Friday")
+		fallthrough
+	case 6:
+		fmt.Println("Saturday")
+		fallthrough
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Invalid Day")
+	}
+
+	fmt.Println()
+
+	// More example
+	notificationType := "sms" // sms, email, push
+
+	switch notificationType {
+	case "sms":
+		fmt.Println("Sent sms")
+	case "email":
+		fmt.Println("Sent email")
+	case "push":
+		fmt.Println("Sent push")
+	default:
+		fmt.Println("Choose a correct notification type.")
+	}
+
+	fmt.Println()
+
+	// Boolean
+
+	// User status (active: true, false)
+	switch isActive := true; isActive {
+	case true:
+		fmt.Println("User is active.")
+	case false:
+		fmt.Println("User in't active.")
+	default:
+		fmt.Println("Unknown")
+	}
+
+	fmt.Println()
 	fmt.Println("----------------")
 }
 
