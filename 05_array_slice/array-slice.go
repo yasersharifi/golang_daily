45f4cfbcae81197cabb1b05542d00fa47a576c52
Yaser Sharifi <yassersharifi74@gmail.com>
2025-11-16 13:19:14 +0330

    feat: array slice

---
+++ 05_array_slice/array-slice.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("--------------")
+	fmt.Println("Array and slice in golang")
+	fmt.Println()
+
+	// Define an array
+
+	// len & cap
+
+	// change the array items value
+
+	// Ellipsis (...)
+
+	// 2D array
+
+	// slice
+
+	// make
+
+	// create a empty slice
+
+	// Add item to slice
+
+	// append
+
+	// remove item
+
+	// copy in slice
+
+	// sort
+
+	// check a slice is nill (slice == nil)
+
+	fmt.Println()
+	fmt.Println("End of array and slice")
+}
