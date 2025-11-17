462dc6ccff1de61aa562a91fed3e01a7bf655a03
Yaser Sharifi <yassersharifi74@gmail.com>
2025-11-17 08:19:17 +0330

    feat: define and change array

--- 05_array_slice/array-slice.go
+++ 05_array_slice/array-slice.go
@@ -8,10 +8,29 @@ func main() {
 	fmt.Println()
 
 	// Define an array
+	arr := [4]int{2, 4, 6, 8}
+	fmt.Println("arr: ", arr)
+	fmt.Println("first item: ", arr[0])
+	fmt.Println("second item: ", arr[1])
+
+	fmt.Println()
 
 	// len & cap
+	fmt.Println("Length of arr: ", len(arr))
+	fmt.Println("Capacity of arr: ", cap(arr))
+
+	fmt.Println()
+
+	// Last item
+	fmt.Println("Last item of arr: ", arr[len(arr) - 1])
 
 	// change the array items value
+	arr[0] = 10
+	arr[1] = 20
+	arr[len(arr) - 1] = 30
+	fmt.Println("arr with new values: ", arr)
+
+	fmt.Println()
 
 	// Ellipsis (...)
 
