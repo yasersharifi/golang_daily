cca6252bd58468c5e1b373b4bb5429b7dce3edb0
Yaser Sharifi <yassersharifi74@gmail.com>
2025-11-25 15:50:13 +0330

    feat: infinite loop in golang

--- 06_loops/loops.go
+++ 06_loops/loops.go
@@ -14,11 +14,22 @@ func main() {
 	}
 	fmt.Println("Sum is: ", sum)
 
-	// while (simulate)
-
-	// Infinite Loops
+	// Infinite Loops - while (simulate) - while(1)
+	j := 0
+	for {
+		fmt.Println("J is: ", j)
+		j += 1
+		if j > 20 {
+			break
+		}
+	}
 
 	// for-range
+	slice := [3]int{1, 3, 5}
+	for i, s := range slice {
+		fmt.Printf("Slice item[%d] is: %d", i, s)
+		fmt.Println()
+	}
 
 	fmt.Println()
 	fmt.Println("End of loops in golang")
